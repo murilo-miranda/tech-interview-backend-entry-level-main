aa678b95e390aca596e657d4db36f5141e643e79
Murilo Miranda <murilomirandamoreno@gmail.com>
2025-07-07 20:01:55 -0300

    Adiciona callbacks para atualizar interacao de carrinhos
    - Garante que mudancas no CartItem atualizem o campo de interacao de Cart
    - Garante que acoes do proprio Cart tambem atualizem o campo de interacao

--- spec/factories/shopping_carts.rb
+++ spec/factories/shopping_carts.rb
@@ -1,10 +1,17 @@
 FactoryBot.define do
   factory :shopping_cart, class: 'Cart' do
     total_price { 0 }
-    last_interaction_at { Time.now }
 
     trait :abandoned do
       status { 'abandoned' }
+    end
+
+    trait :inactivity_of_3_hours do
+      last_interaction_at { 3.hours.ago }
+    end
+
+    trait :inactivity_of_7_days do
+      last_interaction_at { 7.days.ago }
     end
   end
 end
